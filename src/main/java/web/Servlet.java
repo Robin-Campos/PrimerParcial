74b13399b52edf39e05b70f671b142f45fe9da1c
qwerty <lobatocarlos012@gmail.com>
2021-04-10 05:36:38 -0600

    Proyecto terminado

--- src/main/java/web/Servlet.java
+++ src/main/java/web/Servlet.java
@@ -12,29 +12,39 @@ public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
+        int cambio = 0, num1 = 0, num2 = 0;
         String usuario = request.getParameter("name");
         String numero1 = request.getParameter("numero1");
         String numero2 = request.getParameter("numero2");
         String operacion = request.getParameter("Operacion");
 
+        num1 = Integer.parseInt(numero1);
+        num2 = Integer.parseInt(numero2);
+        if (num1 > num2) {
+            cambio = num1;
+            num1 = num2;
+            num2 = cambio;
+        }
         out.print("<html>");
         out.print("<head>");
         out.print("<title>Respuesta</title>");
-        
+        out.print(" <style type =text/css> ");
+        out.print("body {");
+        out.print("background-color: #17223b;");
+        out.print("color:white;}");
+        out.print("");
+        out.print("text-aling: center;");
+        out.print("</style> ");
+
         out.print("</head>");
-        
         out.print("<body>");
-        out.print("<h1>Bienvenido:" + usuario + "</h1>");
-        
+        out.print("<h1>Bienvenido: " + usuario + "</h1>");
         out.print("<p>" + procesos.operacion(numero1, numero2, operacion) + "</p>");
-        out.print("<p>El rango entre: " + numero1 + " y " + numero2 +"</p>");
-        out.print("<div width=90% height=auto>");
-        out.print("<p>"+ procesos.numeroPrimos(numero1, numero2) +"</p>");
-        out.print("</div>");
+        out.print("<p>El rango entre: " + num1 + " y " + num2 + "</p>");
+        out.print("<p> Los numero primos:</p>");
+        out.print("<pNumeros: >" + procesos.numeroPrimos(numero1, numero2) + "</p>");
         out.print("</body>");
         out.print("</html>");
-
     }
 
 }
--- src/main/java/web/procesos.java
+++ src/main/java/web/procesos.java
@@ -1,42 +1,47 @@
-
 package web;
 
 public class procesos {
-  
-    
-    public static String operacion(String numero1,String numero2,String operacion){
+
+    public static String operacion(String numero1, String numero2, String operacion) {
         int numeroUno = Integer.parseInt(numero1);
         int numeroDos = Integer.parseInt(numero2);
         int OperacionHacer = Integer.parseInt(operacion);
         int resultado = 0;
-        
-        if(OperacionHacer == 1){
+
+        if (OperacionHacer == 1) {
             resultado = numeroUno + numeroDos;
             return "El resultado de la Suma es:" + resultado;
-        }else if(OperacionHacer == 2){
+        } else if (OperacionHacer == 2) {
             resultado = numeroUno - numeroDos;
             return "El resultado de la resta es:" + resultado;
-        }else if(OperacionHacer == 3){
+        } else if (OperacionHacer == 3) {
             resultado = numeroUno * numeroDos;
             return "El resultado de la Multiplicacion es:" + resultado;
-        }else if(OperacionHacer == 4){
+        } else if (OperacionHacer == 4) {
             resultado = numeroUno / numeroDos;
             return "El resultado de la divicion es:" + resultado;
-        }   
-        
+        }
+
         return "No se realizo la operacion";
     }
-    
-    
-    public static String numeroPrimos(String numero1, String numero2){
+
+    public static String numeroPrimos(String numero1, String numero2) {
         int numeroUno = Integer.parseInt(numero1);
         int numeroDos = Integer.parseInt(numero2);
+        int cambio;
+
+        if (numeroUno > numeroDos) {
+            cambio = numeroUno;
+            numeroUno = numeroDos;
+            numeroDos = cambio;
+        }
+
         int numerosPrimos[];
         int cantNumeroPrimos = 0;
-        int contador= 0;
-        
+        int contador = 0;
+
         String resultado = "";
-        
+
         for (int numeroEvaluar = numeroUno; numeroEvaluar <= numeroDos; numeroEvaluar++) {
             int conteo = 0;
             for (int numeroDividir = 1; numeroDividir <= numeroEvaluar; numeroDividir++) {
@@ -44,13 +49,13 @@ public class procesos {
                     conteo++;
                 }
             }
-            if (conteo == 2){
+            if (conteo == 2) {
                 cantNumeroPrimos++;
             }
         }
-        
-        numerosPrimos =new int[cantNumeroPrimos];
-        
+
+        numerosPrimos = new int[cantNumeroPrimos];
+
         for (int numeroEvaluar = numeroUno; numeroEvaluar <= numeroDos; numeroEvaluar++) {
             int conteo = 0;
             for (int numeroDividir = 1; numeroDividir <= numeroEvaluar; numeroDividir++) {
@@ -58,18 +63,18 @@ public class procesos {
                     conteo++;
                 }
             }
-            if (conteo == 2){
+            if (conteo == 2) {
                 numerosPrimos[contador] = numeroEvaluar;
                 contador++;
             }
         }
-        
-        for(int numPri = numerosPrimos.length-1; numPri > -1; numPri--){
-            
-            resultado += numerosPrimos[numPri] + ",";
-            
+
+        for (int numPri = numerosPrimos.length - 1; numPri > -1; numPri--) {
+
+            resultado += numerosPrimos[numPri] + " ";
+
         }
-        System.out.println(resultado);
+
         return resultado;
     }
 }
